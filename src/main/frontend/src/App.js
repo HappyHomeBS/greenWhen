6ca342d39f6767679cd1a6ccbfc6a5182ecf148b
BSJ <stairs13@naver.com>
2022-12-06 18:09:57 +0900

    2022.12.06

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,21 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Layout from './layouts/Layout'
 
-function App() {
-   const [hello, setHello] = useState('')
-
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-
-    return (
-        <div>
-            안녕하세요 백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-    );
-
+const App = () => {
+  return (
+    <Layout>
+      <Container style={{ minHeight: "75vh" }}>App</Container>
+    </Layout>
+  )
 }
 
-export default App;
+export default App
