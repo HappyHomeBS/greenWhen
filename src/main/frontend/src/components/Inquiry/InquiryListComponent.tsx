8547945d619d775bf3a8f8a8a52f133d7e8d1db4
green <>
2023-02-10 14:31:48 +0900

    0210 필터링 완성

--- src/main/frontend/src/components/Inquiry/InquiryListComponent.tsx
+++ src/main/frontend/src/components/Inquiry/InquiryListComponent.tsx
@@ -10,17 +10,21 @@ import {InquiryInterface} from '../Inquiry/InquiryInterface';
 import * as InquiryPaging from'../Inquiry/InquiryPaging';
 import { InquirySearch } from "./InquirySearchComponent";
 
+
 const InquiryList: React.FC = (props: any) => {
+    const authCtx = useContext(AuthContext);
+    const token = authCtx.token;
+    const userId = authCtx.userObj.userid;
+    const userRole = authCtx.userObj.role;
     const [currentPage, setCurrentPage] = useState(1);
     const [inquiryList, setInquiryList] = useState<Array<InquiryInterface>>([]); //현재페이지글
     const [totalList, setTotalList] = useState<Array<InquiryInterface>>([]); //전체글
+
     // const [loaded, setLoaded] = useState(false);
     // const [inquiriesPerPage, setInquiresPerPage] = useState(10);
-    const [inquiryCounts, setInquiryCounts] = useState(0); //전체글 숫자
+    // const [inquiryCounts, setInquiryCounts] = useState(0); //전체글 숫자
     const [search, setSearch] = useState<string>("")
-    const [status, setStatus] = useState<string>("")
-    const authCtx = useContext(AuthContext);
-    const token = authCtx.token;
+    const [status, setStatus] = useState<string>("전체")
     const navigate = useNavigate();
     // const moveList = props.setWhich(2)
     
@@ -35,16 +39,20 @@ const InquiryList: React.FC = (props: any) => {
         goPage();
     
     }, [currentPage, totalList])
+
+    useEffect(()=>{
+        statusHandler();
+    }, [status])
         
     const getInquiryList = async () => {
     
         const listData =( (await InquiryService.getInquiryList(token)).data.inquiryList);  
         const newInquiryList = InquiryPaging.GetPostsLoaded(listData, currentPage); // 슬라이스후 현재페이지 글목록
+        setTotalList(listData) // 모든 게시글 리스트 저장
       
         setInquiryList(newInquiryList);
 
         
-        setTotalList(listData) // 모든 게시글 리스트 저장
         // setInquiryCounts(totalList.length) // 불러온 모든 게시글 수 
     }
 
@@ -79,30 +87,38 @@ const InquiryList: React.FC = (props: any) => {
         setSearch('');
     }
 
-    const statusHandler = (e:any) => {
+    const statusChangeHandler=(e:any) => {
         e.preventDefault();
         setStatus(e.target.value);
         console.log(e.target.value);
-        if(status!=='전체'){
-        const filterdData=totalList.filter((inquiry) => inquiry.status===status)
-        console.log(filterdData)
-        setTotalList(filterdData);}
+      
+    }
+
+    const statusHandler = async () => {
+        if(status==='전체'){
+        getInquiryList();
+        }else{
+        const listData =( (await InquiryService.getInquiryList(token)).data.inquiryList)
+        const filterdData=listData.filter((inquiry:any) => inquiry.status===status)
+        console.log('fdata', filterdData)
+        setTotalList(filterdData);
+        setCurrentPage(1);
+        }
     }
 
 
     return (
         
-        <>  
+        <>  {userRole ==='ROLE_ADMIN' &&
         <div className="select_status">
-            
-            <select defaultValue='전체' name='selectStatus' onChange={statusHandler}>
+            <select defaultValue='전체' name='selectStatus' onChange={statusChangeHandler} style={{float:"right", marginRight:"5%"}}>
              <option value='전체'>전  체</option>
              <option value='확인중'>확인중</option>
              <option value='처리중'>처리중</option>
              <option value='답변완료'>답변완료</option>
             </select>
-
         </div>
+            }
             <div className="inquiry_list" style={{margin:"5%"}}>
                 <h2 className="text-center"style={{margin:"1%"}}> 1:1 문의 </h2>
                 <div className="row">
@@ -135,7 +151,7 @@ const InquiryList: React.FC = (props: any) => {
             <div style={{textAlign:"center"}}>
               <InquiryPaging.PageNumbers currentPage={currentPage} totalList={totalList} setCurrentPage={setCurrentPage} />
             </div>
-            <div>
+            <div style={{marginTop:"3%", textAlign:"center"}}>
                 <form onSubmit={e => onSearch(e)}>
                 <input type="text" value={search} placeholder="검색어를 입력하세요." onChange={onChangeSearch}/>
                 <button type='submit'> 검색 </button>
